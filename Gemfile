557715eef777229c0ce836f7817dfbecc88d8fef
Shinichi Segawa <tektoh@gmail.com>
2021-02-09 16:24:51 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+ruby '3.0.0'
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
+gem 'dotenv'
+gem 'puma'
+gem 'sinatra'
+gem 'sinatra-contrib'
---
+++ app.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'bundler'
+Bundler.require
+
+require 'dotenv'
+Dotenv.load
+
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'json'
+require 'net/http'
+
+TOKEN = ENV['TOKEN']
+API_KEY = ENV['API_KEY']
+CLIENT_ID = ENV['CLIENT_ID']
+
+post '/url', provides: :json do
+  params = JSON.parse(request.body.read)
+  raise BadRequest if params['token'] != TOKEN
+
+  uri = URI.parse('https://safebrowsing.googleapis.com/v4/threatMatches:find')
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+
+  headers = {
+    'Content-Type' => 'application/json'
+  }
+
+  body = {
+    'client' => {
+      'clientId' => CLIENT_ID,
+      'clientVersion' => '0.0.1'
+    },
+    'threatInfo' => {
+      'threatTypes' => %w[MALWARE SOCIAL_ENGINEERING UNWANTED_SOFTWARE POTENTIALLY_HARMFUL_APPLICATION],
+      'platformTypes' => ['ALL_PLATFORMS'],
+      'threatEntryTypes' => ['URL'],
+      'threatEntries' => [
+        { 'url' => params['url'].to_s }
+      ]
+    }
+  }.to_json
+
+  res = http.post(uri.path + "?key=#{API_KEY}", body, headers)
+  raise 'API Error' if res.code[0] != '2'
+
+  result = JSON.parse(res.body)
+
+  if result['matches']
+    result['matches'].first['threatType']
+  else
+    'OK'
+  end
+end
